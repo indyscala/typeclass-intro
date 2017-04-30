32532cb3652e299b840e88a131da0fa342c1ac1f
Brad Fritz <brad@fewerhassles.com>
2017-04-30 10:00:44 -0400

    add naive pretty printing example without a type class

---
+++ src/main/scala/org/indyscala/typeclasses/naive/Pretty.scala
@@ -0,0 +1,12 @@
+package org.indyscala.typeclasses.naive
+
+object Pretty {
+  def pp(i: Int): String = s"int $i"
+  def pp(s: String): String = s"str $s"
+}
+
+object Demo extends App {
+  import Pretty._
+  println(pp(123))
+  println(pp("abc"))
+}
