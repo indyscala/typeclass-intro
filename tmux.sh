8bdb035fbb843ef3938c478d7f4da48eb2dfbb7f
Brad Fritz <brad@fewerhassles.com>
2017-04-30 15:18:04 -0400

    add `./tmux.sh close` and attach after running `./tmux.sh`

--- tmux.sh
+++ tmux.sh
@@ -4,11 +4,22 @@ set -e
 
 S=typeclasses
 
+if [ "$1" = "close" ]; then
+    for W in 8 7 6 5 4 3 2 1 0; do
+        tmux select-window -t $W
+        tmux send-keys ':q' C-m
+    done
+    tmux kill-session -t $S
+    exit
+fi
+
 tmux start-server
 tmux new-session -d -s $S
 tmux set-option -g -t $S allow-rename off
 
 for P in a b c d e f g h; do
     tmux new-window -t $S -n $P \; send-keys "vi src/main/scala/org/indyscala/typeclasses/$P/Pretty.scala" C-m
-
 done
+tmux select-window -t 1
+
+tmux attach -t typeclasses
